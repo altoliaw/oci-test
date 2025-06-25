c5e248bd772fa385cecb39acc7a2eb7325cc87eb
Nick Liao <nick.liao@cobrasonic.com>
2025-06-25 17:21:04 +0800

    refactor(native-sizing): Reorganizing the Apps structure and adding a global variable, STATIC_VENDOR_BINARY_DIR_PATH

--- Settings/.Cmake/.systemSupport.cmake
+++ Settings/.Cmake/.systemSupport.cmake
@@ -2,7 +2,7 @@
 # This file sets the OPERATING_SYSTEM variable based on the platform. 
 # If the platform does not match Linux or Windows, it will raise an error (exiting the process).
 if(UNIX)
-    set(OPERATING_SYSTEM 0 CACHE STRING "Operating system flag (string format). 0 for Linux, 1 for Windows")
+    set(OPERATING_SYSTEM 0 CACHE STRING "Operating system flag (string format). 0 for Linux, 1 for Linux")
 elseif(WIN32)
     set(OPERATING_SYSTEM 1 CACHE STRING "Operating system flag (string format). 0 for Linux, 1 for Windows")
 else()
